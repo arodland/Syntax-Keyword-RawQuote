738f05974272d081fe813d5cdb7fbf5a70a04883
Andrew Rodland <andrew@cleverdomain.org>
2017-02-19 23:49:08 -0500

    Flesh out POD

--- lib/Syntax/Feature/RawQuote.pm
+++ lib/Syntax/Feature/RawQuote.pm
@@ -22,7 +22,29 @@ __END__
 
 =head1 NAME
 
-Syntax::Keyword::RawQuote - do raw quotes
+Syntax::Feature::RawQuote - A raw quote operator for Perl ('use syntax' flavor)
+
+=head1 SYNOPSIS
+
+    use syntax 'raw_quote';
+    say r`I keep all of my files in \\yourserver\private`;
+
+    use syntax raw_quote => { -as => "qraw" };
+    say qraw[Maybe the \r\ keyword is too risky?];
+
+This library provides an interface to L<Syntax::Keyword::RawQuote> for the
+L<syntax> module. You may also see L<Syntax::Keyword::RawQuote> directly. See
+that module for more information.
+
+=head1 METHODS
+
+=head2 install
+
+Called by L<syntax> to enable the module when you do C<use syntax 'raw_quote'>.
+
+=head2 uninstall
+
+Called by L<syntax> to disable the module when you do C<no syntax 'raw_quote'>.
 
 =head1 AUTHOR
 
@@ -32,4 +54,5 @@ Andrew Rodland <arodland@cpan.org>
 
 Copyright (c) Andrew Rodland.
 
-This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
--- lib/Syntax/Keyword/RawQuote.pm
+++ lib/Syntax/Keyword/RawQuote.pm
@@ -30,7 +30,67 @@ __END__
 
 =head1 NAME
 
-Syntax::Keyword::RawQuote - do raw quotes
+Syntax::Keyword::RawQuote - A raw quote operator for Perl
+
+=head1 SYNOPSIS
+
+    use Syntax::Keyword::RawQuote;
+    say r`I keep all of my files in \\yourserver\private`;
+
+    use Syntax::Keyword::RawQuote -as => "qraw";
+    say qraw[Maybe the \r\ keyword is too risky?];
+
+This library provides an absolutely uninterpreted (raw) quote operator for
+Perl, as seen in some other programming languages. While Perl single-quoted
+strings are nearly uninterpreted, they still treat the sequences C<\'> and
+C<\\> specially, allowing a single quote to be included in the string.
+The C<q> operator behaves similarly, allowing the closing delimiter to be
+backslashed, and treating C<\\> as a single backslash. A raw string, in
+contrast, treats I<every> character literally, and ends at the first
+occurrence of the closing delimiter, no matter what.
+
+=head1 SYNTAX
+
+By default, the raw quote operator will be installed as C<r> in the lexical
+scope where this module is imported. If that name is inconvenient, you can
+choose another one by providing the C<-as> option in the C<use> statement.
+
+The operator behaves like other quote-like operators (see
+L<perlop/Quote and Quote-like Operators>). The first non-whitespace character
+after the operator is taken as the opening delimiter. If the opening
+delimiter is one of the ASCII left bracket characters (one of C<< ( [ < { >>),
+then the closing delimiter is the matching right bracket (from
+C<< ) ]] > } >>, otherwise it is the same as the opening delimiter. This
+choice of delimiters allows you to choose a character that won't appear
+inside the string.
+
+Since editors' syntax highlighting features will probably not recognize the
+existence of this module, you may want to use an existing quote character to
+avoid confusing them. C<r""> and C<r''> are obvious choices, but C<r``>
+(similar to Go) has the advantage that backticks hardly ever occur inside of
+quoted strings, and it is visually distinct.
+
+=head1 METHODS
+
+=head2 import
+
+Enables the raw quote keyword in the current lexical scope when called during
+compilation (C<use Syntax::Feature::RawQuote>). If the C<-as> keyword argument
+is provided, it will be used as the keyword name, otherwise C<"r"> is used.
+
+=head2 unimport
+
+Disables the raw quote keyword in the current lexical scope when called
+during compilation (C<no Syntax::Feature::RawQuote>). If the C<-as> keyword is
+provided, disables that keyword specifically; otherwise, all keywords
+installed will be disabled.
+
+=head1 syntax.pm SUPPORT
+
+A module L<Syntax::Feature::RawQuote> is also provided, if you prefer the
+form C<use syntax 'raw_quote'>. Provides all of the same features and
+options, except that it is not possible to
+C<< no syntax raw_quote => { -as => 'foo' } >>.
 
 =head1 AUTHOR
 
@@ -40,4 +100,5 @@ Andrew Rodland <arodland@cpan.org>
 
 Copyright (c) Andrew Rodland.
 
-This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
